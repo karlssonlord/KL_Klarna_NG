96844744f3008f89cdee9c2fbcd6bb6ce3e744c7
Martin Svangren <martin@masv.net>
2013-11-08 18:04:17 +0100

    Fleshing out the Invoice class
    This sets up the Invoice payment method with flags for supported
    features and adds the methods that need to be implemented.
    
    Partial capture is left out for now, although the Klarna API does
    support modifying reservations. We might implement this later, but we
    should focus on implementing full captures first.
    
    Signed-off-by: Martin Svangren <martin@masv.net>

--- app/code/community/KL/Klarna/Model/Invoice.php
+++ app/code/community/KL/Klarna/Model/Invoice.php
@@ -2,4 +2,62 @@
 class KL_Klarna_Model_Invoice extends Mage_Payment_Model_Method_Abstract
 {
     protected $_code = "klarna_invoice";
+
+    protected $_isGateway               = true;
+    protected $_canAuthorize            = true;
+    protected $_canCapture              = true;
+    protected $_canCapturePartial       = false;
+    protected $_canRefund               = true;
+    protected $_canRefundInvoicePartial = false;
+    protected $_canVoid                 = true;
+    protected $_canUseInternal          = true;
+    protected $_canUseCheckout          = true;
+    protected $_canUseForMultishipping  = false;
+    protected $_canSaveCc               = false;
+    protected $_canFetchTransactionInfo = false;
+
+    public function canUseForCountry($countryCode)
+    {
+        $this->_debug(array("canUseForCountry", $countryCode));
+        return parent::canUseForCountry($countryCode);
+    }
+
+    public function canUseForCurrency($currencyCode)
+    {
+        $this->_debug(array("canUseForCurrency", $currencyCode));
+        return parent::canUseForCurrency($currencyCode);
+    }
+
+    public function authorize(Varien_Object $payment, $amount)
+    {
+        $this->_debug("authorize");
+        return parent::authorize($payment, $amount);
+    }
+
+    public function capture(Varien_Object $payment, $amount)
+    {
+        $this->_debug("capture");
+        return parent::capture($payment, $amount);
+    }
+
+    public function cancel(Varien_Object $payment)
+    {
+        $this->_debug("cancel");
+        return parent::cancel($payment);
+    }
+
+    public function void(Varien_Object $payment)
+    {
+        $this->_debug("void");
+        return parent::void($payment);
+    }
+
+    public function refund(Varien_Object $payment, $amount)
+    {
+        /*
+         * NOTE: Online refund requires a transaction ID!
+         */
+        $this->_debug("refund");
+        return parent::refund($payment, $amount);
+    }
 }
