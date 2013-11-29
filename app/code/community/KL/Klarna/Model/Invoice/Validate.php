104c029df0fae86a3fa5aed6cba0c598302628ce
Peter Luotsinen <peter@karlssonlord.com>
2013-11-29 16:32:41 +0100

    Refactor validate function

--- app/code/community/KL/Klarna/Model/Invoice/Validate.php
+++ app/code/community/KL/Klarna/Model/Invoice/Validate.php
@@ -2,7 +2,21 @@
 
 class KL_Klarna_Model_Invoice_Validate extends KL_Klarna_Model_Invoice_Abstract
 {
+
+    /**
+     * Validation
+     *
+     * @throws Exception
+     */
     public function validate()
+    {
+        $this->validateCountryCodeAndCurrency();
+    }
+
+    /**
+     * Checks if the currency and country match, else throw a exception
+     */
+    protected function validateCountryCodeAndCurrency()
     {
         $paymentInfo    = $this->getPayment();
         if ($paymentInfo instanceof Mage_Sales_Model_Order_Payment) {
