323eee3d71c58d419a90fb20ca26464d2f576de6
Robert Lord <robert@codepeak.se>
2013-12-10 23:07:38 +0100

    Terms and conditions and payment logotypes

--- app/code/community/KL/Klarna/Block/Form.php
+++ app/code/community/KL/Klarna/Block/Form.php
@@ -2,6 +2,16 @@
 
 class KL_Klarna_Block_Form extends Mage_Payment_Block_Form {
 
+    public function getCurrentCountry()
+    {
+        return 'SE';
+    }
+
+    public function getEid()
+    {
+        return Mage::helper('klarna')->getConfig('merchant_id');
+    }
+
 }
 
 
--- app/code/community/KL/Klarna/Model/Payment/Specpayment.php
+++ app/code/community/KL/Klarna/Model/Payment/Specpayment.php
@@ -6,13 +6,21 @@ class KL_Klarna_Model_Payment_Specpayment extends KL_Klarna_Model_Payment_Abstra
     protected $_formBlockType = 'klarna/specpayment_form';
     protected $_infoBlockType = 'klarna/specpayment_info';
 
-    public function getTitle()
+    public function __construct()
+    {
+        // disable if no pclass exists
+        $this->_isGateway = false;
+        $this->_canUseCheckout = false;
+    }
+
+    public function agetTitle()
     {
         /**
          * @todo Magic for setting the current campaign name here and also have the method removed if no campaign exists
          */
-        $this->_code = null;
-        $this->_canUseCheckout = false;
+#        $this->_code = null;
+        #
+
 
         return $this->__('No campaign is currently set');
     }
