0a620576f16da4c4d539190598045e093032ab2e
Robert Lord <robert@codepeak.se>
2013-12-12 11:28:11 +0100

    Javascript for updating addresses in frontend

--- skin/frontend/base/default/js/kl_klarna.js
+++ skin/frontend/base/default/js/kl_klarna.js
@@ -1,14 +1,104 @@
 // Fetch address and populare social security number
 function fetchKlarnaAddress(fieldId) {
 
-    // Populate social security fields
+    // Populate social security fields with the new social security number
     $$('.klarna_ssn').each(function(el) {
         $(el).value = $(fieldId).value;
     });
 
+    // Setup query url
+    var queryUrl = addressEndpoint + 'ssn/' + $(fieldId).value;
+
+    // Query endpoint for fetching address
+    new Ajax.Request(queryUrl, {
+        method:'get',
+        onSuccess: function(transport){
+
+            // Fix the returned data
+            var json = transport.responseText.evalJSON();
+
+            // Setup empty variables that we will use
+            var hiddenAddressFields = '';
+            var selectBox = '';
+            var checked = true;
+
+            $(json).each(function(address){
+
+                console.log(address);
+
+                // Setup the address line
+                var addressLine =
+                    address.fname + ' ' +
+                    address.lname + ', ' +
+                    address.street + ', ' +
+                    address.zip + ' ' + address.city;
+
+                // Build the hidden address fields with hash
+                $H(address).each(function(addressLine) {
+                    if (addressLine.key !== 'hash') {
+                        hiddenAddressFields = hiddenAddressFields +'<input type="hidden" id="' + address.hash + '_' + addressLine.key + '" value="' + addressLine.value + '"/>';
+                    }
+                });
+
+                // Setup the checkboxes with addresses
+                selectBox = selectBox + '<label for="klarna_address_key_' + address.hash + '">';
+                selectBox = selectBox + '<input onClick="klarnaChangeAddress();" type="radio" id="klarna_address_key_' + address.hash + '" name="klarna_address_key" value="' + address.hash + '" style="display: block !important"';
+                if (checked === true) {
+                    selectBox = selectBox + ' checked="checked"';
+                    checked = false;
+                }
+                selectBox = selectBox + '/> ' + addressLine + '</label>';
+
+            });
+
+            // Set the HTML fields
+            $('klarna_select_address').update(hiddenAddressFields + selectBox);
+
+            // Set the checked address
+            klarnaChangeAddress();
+
+            // Check how many addresses that were returned
+            if (json.length == 1) {
+                // Only one, no need for end user to select, populate using the one we found
+            }
+
+        }
+    });
+
+}
+
+function klarnaChangeAddress()
+{
+    // Fetch address hash
+    var addressHash = $$('input:checked[type=radio][name=klarna_address_key]')[0].value;
+
+    // Setup query url
+    var queryUrl = updateAddressEndpoint + 'address_key/' + addressHash;
+
+    // Compose array with address data
+    var magentoAddress = {
+        'shipping[firstname]': $(addressHash + '_fname').value,
+        'shipping[lastname]':  $(addressHash + '_lname').value,
+        'shipping[street][]':  $(addressHash + '_street').value,
+        'shipping[postcode]':  $(addressHash + '_zip').value,
+        'shipping[city]':      $(addressHash + '_city').value
+    };
+
     // Check if Less Friction is used
     if( typeof Checkout !== 'undefined' ) {
-        // @todo Trigger reload of elements
+        // Inject shipping address
+        shippingAddress.inject($H(magentoAddress));
+        billingAddress.inject($H(magentoAddress));
+    } else {
+        // @todo Update standard Magento fields
     }
+
+    // Query endpoint for updating address
+    new Ajax.Request(queryUrl, {
+        method:'get',
+        onSuccess: function(transport){
+            // What should we do when the quote is updated?
+        }
+    });
 
 }
