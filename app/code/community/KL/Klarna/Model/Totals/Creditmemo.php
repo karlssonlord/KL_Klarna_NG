6dc54cf3cd195eed2d3a9aca73c0ba14688b66a0
Robert Lord <robert@codepeak.se>
2014-01-07 11:04:30 +0100

    Fixed refund error

--- app/code/community/KL/Klarna/Model/Totals/Creditmemo.php
+++ app/code/community/KL/Klarna/Model/Totals/Creditmemo.php
@@ -1,5 +1,5 @@
 <?php
 
-class KL_Klarna_Model_Totals_Creditmemo extends KL_Klarna_Model_Totals_Invoice {
+class KL_Klarna_Model_Totals_Creditmemo extends Mage_Sales_Model_Order_Creditmemo_Total_Abstract {
 
 }
--- app/code/community/KL/Klarna/Model/Totals/Invoice.php
+++ app/code/community/KL/Klarna/Model/Totals/Invoice.php
@@ -12,12 +12,9 @@ class KL_Klarna_Model_Totals_Invoice extends Mage_Sales_Model_Order_Invoice_Tota
 
         $baseMyTotal = $order->getBaseKlarnaTotal();
 
-        Mage::log($myTotal . ' xx ' . $baseMyTotal);
-
         $invoice->setGrandTotal($invoice->getGrandTotal() + $myTotal);
         $invoice->setBaseGrandTotal($invoice->getBaseGrandTotal() + $baseMyTotal);
 
-        Mage::log('invoice collected');
         return $this;
     }
 
