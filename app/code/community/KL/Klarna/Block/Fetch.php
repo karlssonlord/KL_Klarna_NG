c2d0d0137159a9669c889dac0cc6b111140ce46f
Andreas Karlsson <andreas.karlsson@indiebytes.se>
2014-01-20 15:53:18 +0100

    Add endpoint methods to the fetch block class

--- app/code/community/KL/Klarna/Block/Fetch.php
+++ app/code/community/KL/Klarna/Block/Fetch.php
@@ -1,5 +1,23 @@
 <?php
+class KL_Klarna_Block_Fetch extends Mage_Core_Block_Template
+{
+    /**
+     * Get URL for fetching address from SSN
+     *
+     * @return string
+     */
+    public function getFetchAddressEndpoint()
+    {
+        return Mage::getUrl('klarna/address/get');
+    }
 
-class KL_Klarna_Block_Fetch extends Mage_Core_Block_Template {
-
+    /**
+     * Get URL for updating address object
+     *
+     * @return string
+     */
+    public function getUpdateAddressEndpoint()
+    {
+        return Mage::getUrl('klarna/address/update');
+    }
 }
