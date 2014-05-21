9bb983040b5cc7b8bf0052ac942fdff89dc8e81e
Robert Lord <robert@codepeak.se>
2014-05-21 10:33:21 +0200

    Checkout block

--- app/code/community/KL/Klarna/Block/Checkout/Klarna/Checkout.php
+++ app/code/community/KL/Klarna/Block/Checkout/Klarna/Checkout.php
@@ -5,4 +5,20 @@
  */
 class KL_Klarna_Block_Checkout_Klarna_Checkout extends Mage_Core_Block_Template {
 
+    /**
+     * Fetch Klarna HTML
+     */
+    public function getKlarnaHTML()
+    {
+        /**
+         * Fetch the Klarna model
+         */
+        $model = Mage::getModel('klarna/klarnacheckout');
+
+        /**
+         * Create the order and return the checkout HTML
+         */
+        return $model->createOrder();
+    }
+
 }
