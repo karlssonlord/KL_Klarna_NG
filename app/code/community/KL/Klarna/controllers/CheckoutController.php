6b12d41ead1ff7f30c2e6564d7fad9ff1d188e93
tykhon <tykhon@karlssonlord.com>
2014-09-29 17:18:51 +0200

    https://trello.com/c/IK6kWCzr/344-add-validation-callback-klarna-checkout

--- app/code/community/KL/Klarna/controllers/CheckoutController.php
+++ app/code/community/KL/Klarna/controllers/CheckoutController.php
@@ -106,4 +106,105 @@ class KL_Klarna_CheckoutController extends Mage_Checkout_OnepageController {
         echo $orderId;
         die();
     }
+
+    /**
+     * Validates the Klarna order to make sure its still available for purchase
+     *
+     * @return void
+     */
+    public function validateAction()
+    {
+        $error = false;
+        $postData = json_decode(file_get_contents('php://input'), true);
+        $quoteId = $postData['merchant_reference']['orderid2'];
+        $quote = Mage::getModel('sales/quote')->load($quoteId);
+        $isStockArray = array();
+
+        if(strtolower($quote->getQuoteCurrencyCode()) !== strtolower($postData['purchase_currency'])) {
+            Mage::helper('klarna/log')->log(
+                $quote,
+                'Currency mismatch. Given ' . strtolower($postData['purchase_currency']) . ' but should be ' .
+                strtolower($quote->getQuoteCurrencyCode())
+            );
+            $error = true;
+        }
+
+        if(((float)$quote->getGrandTotal()*100) !== (float)$postData['cart']['total_price_including_tax']) {
+            Mage::helper('klarna/log')->log(
+                $quote,
+                'Totals are different. Given ' . (float)$postData['cart']['total_price_including_tax']
+                . ' but should be ' . ((float)$quote->getGrandTotal()*100)
+            );
+            $error = true;
+
+        }
+
+        $klarnaItems = array();
+        //Reorginizing the array for the following easy search
+        foreach($postData['cart']['items'] as $klarnaItem) {
+            if($klarnaItem['type'] === 'physical') {
+                $klarnaItems[$klarnaItem['reference']] = $klarnaItem;
+            }
+        }
+
+        foreach($quote->getAllVisibleItems() as $quoteItem) {
+            if(empty($klarnaItems[$quoteItem->getSku()])) {
+                Mage::helper('klarna/log')->log(
+                    $quote,
+                    'Item with SKU ' . $quoteItem->getSku()
+                    . ' doesn\'t exist in the Klarna cart ' . ((float)$quote->getGrandTotal()*100)
+                );
+                $error = true;
+            } else {
+                if(!Mage::getModel('catalog/product')->load($quoteItem->getProductId())->isSalable()) {
+                    $isStockArray = array('is_stock' => 1);
+                    Mage::helper('klarna/log')->log(
+                        $quote,
+                        'Item with SKU ' . $quoteItem->getSku()
+                        . ' is not salable ' . ((float)$quote->getGrandTotal()*100)
+                    );
+                    $error = true;
+                } else {
+                    unset($klarnaItems[$quoteItem->getSku()]);
+                }
+            }
+        }
+
+        if(count($klarnaItems) > 0) {
+            Mage::helper('klarna/log')->log(
+                $quote,
+                'Klarna cart and Magento quote do not match. Klarna cart contains more products than Magento quote'
+            );
+            $error = true;
+        }
+
+        if($error) {
+            $this->getResponse()->setRedirect(Mage::getUrl('klarna/checkout/failure', $isStockArray), 303);
+            Mage::helper('klarna/log')->log(
+                $quote,
+                var_export($postData, true)
+            );
+        } else {
+            $this->getResponse()->setHttpResponseCode(200);
+        }
+
+        Mage::helper('klarna/log')->log(
+            $quote,
+            'Check is finished'
+        );
+
+    }
+
+    /**
+     * Provides error message if necessary and redirects to the checkout page showing the error message.
+     *
+     * @return void
+     */
+    public function failureAction()
+    {
+        if(!$this->getRequest()->getParam('is_stock')) {
+            Mage::getSingleton("core/session")->addError('We could not fulfil your order. Please try again or contact our support.');
+        }
+        $this->getResponse()->setRedirect(Mage::getUrl('klarna/checkout'));
+    }
 }
