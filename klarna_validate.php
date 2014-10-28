5810f0f81322c9b38843850725e3bdbc09124720
tykhon <tykhon@karlssonlord.com>
2014-10-28 14:02:37 +0100

    Improved klarna_vaidate.php. Now it checks qty and if the product is enabled

--- klarna_validate.php
+++ klarna_validate.php
@@ -10,9 +10,7 @@
  */
 class KlarnaValidate
 {
-
     protected $_data;
-
     protected $_connection;
 
     /**
@@ -20,9 +18,7 @@ class KlarnaValidate
      */
     public function __construct()
     {
-        $json = '{"id":"149428B7A280A0CF337726C0000","merchant_reference":{"orderid2":"648336"},"purchase_country":"se","purchase_currency":"sek","locale":"sv-se","status":"checkout_incomplete","started_at":"2014-10-24T16:24:37+02:00","last_modified_at":"2014-10-24T16:24:37+02:00","cart":{"total_price_excluding_tax":6000,"total_tax_amount":1500,"total_price_including_tax":7500,"items":[{"reference":"209996813","name":"Renske Kalkon, Anka & Ris 395g","quantity":1,"unit_price":2600,"tax_rate":2500,"discount_rate":0,"type":"physical","total_price_including_tax":2600,"total_price_excluding_tax":2080,"total_tax_amount":520},{"reference":"matrixrate_matrixrate_220","name":"Frakts\\u00e4tt - 1 Hempaket utan kvittens","quantity":1,"unit_price":4900,"tax_rate":2500,"discount_rate":0,"type":"shipping_fee","total_price_including_tax":4900,"total_price_excluding_tax":3920,"total_tax_amount":980}]},"customer":{"type":"person","date_of_birth":"1941-03-21","gender":"female"},"shipping_address":{"given_name":"Testperson-se","family_name":"Approved","street_address":"St\\u00e5rgatan 1","postal_code":"12345","city":"Ankeborg","country":"se","email":"checkout-se@testdrive.klarna.com","phone":"070 111 11 11"},"billing_address":{"given_name":"Testperson-se","family_name":"Approved","street_address":"St\\u00e5rgatan 1","postal_code":"12345","city":"Ankeborg","country":"se","email":"checkout-se@testdrive.klarna.com","phone":"070 111 11 11"},"gui":{"layout":"desktop","options":["disable_autofocus"],"snippet":"<div id=\\"klarna-checkout-container\\" style=\\"overflow-x: hidden;\\"><script type=\\"text/javascript\\">/* <![CDATA[ */(function(w,k,i,d,u,n,c){w[k]=w[k]||function(){(w[k].q=w[k].q||[]).push(arguments)};w[k].config={container:w.document.getElementById(i),TESTDRIVE:true,ORDER_URL:\'https://checkout.testdrive.klarna.com/checkout/orders/149428B7A280A0CF337726C0000\',AUTH_HEADER:\'KlarnaCheckout K0YCI5s7kx5XbOoZjnoS\',LAYOUT:\'desktop\',LOCALE:\'sv-se\',ORDER_STATUS:\'checkout_incomplete\',MERCHANT_TAC_URI:\'http://local.animail.se/\',MERCHANT_TAC_TITLE:\'Karlsson & Lord\',MERCHANT_NAME:\'Karlsson & Lord\',GUI_OPTIONS:[\'disable_autofocus\'],ALLOW_SEPARATE_SHIPPING_ADDRESS:false,NATIONAL_IDENTIFICATION_NUMBER_MANDATORY:false,ANALYTICS:\'UA-36053137-1\',PHONE_MANDATORY:false,PACKSTATION_ENABLED:false,PURCHASE_COUNTRY:\'swe\',PURCHASE_CURRENCY:\'sek\',BOOTSTRAP_SRC:u};n=d.createElement(\'script\');c=d.getElementById(i);n.async=!0;n.src=u;c.insertBefore(n,c.firstChild);})(this,\'_klarnaCheckout\',\'klarna-checkout-container\',document,\'https://checkout.testdrive.klarna.com/141010-5465f7b/checkout.bootstrap.js\');/* ]]> */</script><noscript>Please <a href=\\"http://enable-javascript.com\\">enable JavaScript</a>.</noscript></div>"},"merchant":{"id":"862","terms_uri":"http://local.animail.se/","checkout_uri":"http://local.animail.se/klarna/checkout/","confirmation_uri":"http://local.animail.se/klarna/checkout/success/","push_uri":"http://local.animail.se/klarna/checkout/push/?klarna_order=https%3a%2f%2fcheckout.testdrive.klarna.com%2fcheckout%2forders%2f149428B7A280A0CF337726C0000","validation_uri":"https://tykhon2.pagekite.me/klarna/checkout/validate"}}';
-        $this->_data = json_decode($json, true);
-//        $this->_data = json_decode(file_get_contents('php://input'), true);
+        $this->_data = json_decode(file_get_contents('php://input'), true);
     }
 
     /**
@@ -30,7 +26,6 @@ class KlarnaValidate
      */
     public function run()
     {
-        $error = false;
         /**
          * Make sure quote id exists
          */
@@ -54,12 +49,12 @@ class KlarnaValidate
              * Mage_CatalogInventory_Model_Stock_Status::STATUS_IN_STOCK
              */
             $quoteItems = $this->query(
-                'SELECT * FROM `sales_flat_quote_item` sfqi
+                'SELECT *, css.qty as available_qty, sfqi.qty as ordered_qty FROM `sales_flat_quote_item` sfqi
                  LEFT JOIN sales_flat_quote sfq ON (sfqi.`quote_id` = sfq.`entity_id`)
-                 LEFT JOIN  core_store cs ON (sfq.`store_id` = cs.`store_id`)
+                 LEFT JOIN core_store cs ON (sfq.`store_id` = cs.`store_id`)
                  LEFT JOIN cataloginventory_stock_status css
                     ON (css.`website_id` = cs.`website_id` AND css.`product_id` = sfqi.`product_id`)
-                WHERE sfqi.`quote_id` = \'' . $this->getQuoteId() . '\' AND css.`stock_id` = 1 AND ISNULL(sfqi.`parent_item_id`)'
+                WHERE sfqi.`quote_id` = \'' . $this->getQuoteId() . '\' AND css.`stock_id` = 1'
             );
 
             /**
@@ -67,22 +62,78 @@ class KlarnaValidate
              */
             while ($quoteItem = mysql_fetch_object($quoteItems)) {
 
-                if(empty($klarnaItems[$quoteItem->sku])) {
+                if(empty($quoteItem->parent_item_id) && empty($klarnaItems[$quoteItem->sku])) {
+                    /*
+                     * Check if quote product exists in Klarna order
+                     */
                     $errorMessage = '[QuoteID:' . $this->getQuoteId() . '] Item with SKU ' . $quoteItem->sku
                         . ' doesn\'t exist in the Klarna cart';
                     $errorMessages[] = $errorMessage;
                 } else {
                     if(!(int)$quoteItem->stock_status) {
+                        /*
+                         * Check if product has stock_status = 1
+                         */
                         $errorMessage = '[QuoteID:' . $this->getQuoteId() . '] Item with SKU ' . $quoteItem->sku
                             . ' is not salable.';
                         $errorMessages[] = $errorMessage;
                         $isStock = '?is_stock=1';
+                    } elseif(
+                        /*
+                         * Check if available quantity is > than ordered
+                         */
+                        $quoteItem->product_type == 'simple' &&
+                        (int)$quoteItem->available_qty < (int)$quoteItem->ordered_qty
+                    ) {
+                        $errorMessage = '[QuoteID:' . $this->getQuoteId() . '] Item with SKU ' . $quoteItem->sku
+                            . ' has available qty = "' . (int)$quoteItem->available_qty . '" but ordered = "' .
+                            (int)$quoteItem->ordered_qty . '".';
+                        $errorMessages[] = $errorMessage;
+                        $isStock = '?is_stock=1';
+                    } else {
+                        /*
+                         * Check if quote product is enabled
+                         */
+                        $statusAttrbutes = $this->query(
+                            'SELECT *, catalog_product_entity_int.value as statusValue FROM eav_attribute
+                                LEFT JOIN eav_entity_type ON (
+                                    eav_attribute.entity_type_id = eav_entity_type.entity_type_id
+                                )
+                                LEFT JOIN catalog_product_entity_int ON (
+                                    catalog_product_entity_int.entity_type_id = eav_entity_type.entity_type_id
+                                    AND catalog_product_entity_int.attribute_id = eav_attribute.attribute_id
+                                )
+                            WHERE
+                                eav_attribute.attribute_code = "status"
+                                AND  eav_entity_type.entity_type_code = "catalog_product"
+                                AND catalog_product_entity_int.entity_id = ' . $quoteItem->product_id . '
+                            ORDER BY catalog_product_entity_int.store_id ASC'
+                        );
+
+                        $disabledProduct = false;
+                        while ($statusAttrbute = mysql_fetch_object($statusAttrbutes)) {
+                            /*
+                             * Mage_Catalog_Model_Product_Status::STATUS_ENABLED
+                             * Mage_Catalog_Model_Product_Status::STATUS_DISABLED
+                             */
+                            if($statusAttrbute->store_id == 0 && $statusAttrbute->statusValue == 2) {
+                                $disabledProduct = true;
+                            } elseif($statusAttrbute->store_id == $quoteItem->store_id && $statusAttrbute->statusValue == 2) {
+                                $disabledProduct = true;
+                            }
+                        }
+                        if($disabledProduct) {
+                            $errorMessage = '[QuoteID:' . $this->getQuoteId() . '] Item with SKU ' . $quoteItem->sku
+                                . ' is disabled.';
+                            $errorMessages[] = $errorMessage;
+                        }
                     }
                     unset($klarnaItems[$quoteItem->sku]);
                 }
                 $currencyCode = $quoteItem->quote_currency_code;
                 $websiteId = $quoteItem->website_id;
             }
+
             if(!isset($websiteId)) {
                 $errorMessage = '[QuoteID:' . $this->getQuoteId() . '] Quote has no items. Quote is empty.';
                 $errorMessages[] = $errorMessage;
@@ -92,8 +143,8 @@ class KlarnaValidate
              * Currency code check
              */
             if(strtolower($currencyCode) !== strtolower($this->_data['purchase_currency'])) {
-                $errorMessage = '[QuoteID:' . $this->getQuoteId() . '] Currency mismatch. Given ' . strtolower($this->_data['purchase_currency']) .
-                    ' but should be ' . strtolower($currencyCode);
+                $errorMessage = '[QuoteID:' . $this->getQuoteId() . '] Currency mismatch. Given ' .
+                    strtolower($this->_data['purchase_currency']) . ' but should be ' . strtolower($currencyCode);
                 $errorMessages[] = $errorMessage;
             }
 
@@ -101,11 +152,10 @@ class KlarnaValidate
              * Check if carts match
              */
             if(count($klarnaItems) > 0) {
-                $errorMessage = '[QuoteID:' . $this->getQuoteId() . '] Klarna cart and Magento quote do not match. Klarna cart contains more products' .
-                    ' than Magento quote';
+                $errorMessage = '[QuoteID:' . $this->getQuoteId() . '] Klarna cart and Magento quote do not match. ' .
+                    'Klarna cart contains more products than Magento quote';
                 $errorMessages[] = $errorMessage;
             }
-
 
         } else {
             $errorMessages[] = 'Missing quote ID';
@@ -135,7 +185,7 @@ class KlarnaValidate
             }
 
             $this->error($errorMessages, $websiteId);
-            
+
             header('HTTP/1.1 303 See Other');
             header('Location: ' . $theUrl . 'klarna/checkout/failure' . $isStock);
         } else {
