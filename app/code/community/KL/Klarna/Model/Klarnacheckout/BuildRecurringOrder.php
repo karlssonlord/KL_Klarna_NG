e2846e3d203f4f1908948ee34c36fd93d9a5ab88
David Wickström <davidwickstrom@gmail.com>
2015-01-30 19:11:51 +0100

    More work on the recurring order builder

--- app/code/community/KL/Klarna/Model/Klarnacheckout/BuildRecurringOrder.php
+++ app/code/community/KL/Klarna/Model/Klarnacheckout/BuildRecurringOrder.php
@@ -2,9 +2,20 @@
 
 class KL_Klarna_Model_Klarnacheckout_BuildRecurringOrder
 {
-    public function build($quote)
+    /**
+     * @var KL_Subscriber_Model_Quote
+     */
+    private $quote;
+
+    /**
+     * @param KL_Subscriber_Model_Quote $quote
+     * @return array
+     */
+    public function build(KL_Subscriber_Model_Quote $quote)
     {
-        $items = $this->prepareOrderItems($quote);
+        $this->setQuote($quote);
+
+        $items = $this->prepareOrderItems();
 
         return $this->createNewOrder($items);
     }
@@ -44,21 +55,27 @@ class KL_Klarna_Model_Klarnacheckout_BuildRecurringOrder
         return $klarnaData;
     }
 
-    private function prepareOrderItems($quote)
+    /**
+     * @return array
+     */
+    private function prepareOrderItems()
     {
-        $items = $this->addQuoteItems($quote);
+        $items = $this->addQuoteItems();
         $items = $this->addShippingDetails($items);
         $items = $this->handleDiscounts($items);
 
         return $items;
     }
 
-    private function addQuoteItems($quote)
+    /**
+     * @return array
+     */
+    private function addQuoteItems()
     {
         $items = array();
 
         // Add all visible items from quote
-        foreach ($quote->getAllVisibleItems() as $item) {
+        foreach ($this->quote->getAllVisibleItems() as $item) {
             $items[] = Mage::getModel('klarna/klarnacheckout_item')->build($item);
         }
 
@@ -66,19 +83,21 @@ class KL_Klarna_Model_Klarnacheckout_BuildRecurringOrder
     }
 
     /**
+     *  Add shipping method and the cost
+     *
      * @param $items
      * @return array
      */
     private function addShippingDetails($items)
     {
-        /**
-         * Add shipping method and the cost
-         */
         $shipping = Mage::getModel('klarna/klarnacheckout_shipping')->build();
+
         if ($shipping) {
             $items[] = $shipping;
+
             return $items;
         }
+
         return $items;
     }
 
@@ -88,14 +107,13 @@ class KL_Klarna_Model_Klarnacheckout_BuildRecurringOrder
      */
     private function handleDiscounts($items)
     {
-        /**
-         * Handle discounts
-         */
         $discounts = Mage::getModel('klarna/klarnacheckout_discount')->build($this->getQuote());
+
         if ($discounts) {
             $items[] = $discounts;
             return $items;
         }
+
         return $items;
     }
 
@@ -109,7 +127,7 @@ class KL_Klarna_Model_Klarnacheckout_BuildRecurringOrder
     {
         // TODO: Fetch current user
         // Load the user by taking the customer_id off the subscription object
-        $currentUser = '';
+        $currentUser = $this->quote->getCustomer();
 
         // Make sure the variable in the array is defined
         if (!isset($klarnaData['shipping_address'])) {
@@ -117,8 +135,16 @@ class KL_Klarna_Model_Klarnacheckout_BuildRecurringOrder
         }
 
         // Set the e-mail
-        $klarnaData['shipping_address']['email'] = $currentUser->getEmail();
+        // TODO: maybe fallback to getting the email off the customer object if the one on the quote is empty?
+        $klarnaData['shipping_address']['email'] = $this->quote->getCustomerEmail();
 
+        // TODO: Think about where we should get the address from. Template quote or just quote?
+        // TODO: What if the address returns invalid from Klarna, do we try another address and notify the customer?
+        // I suppose we could fetch the shipping address from the original order.
+        // Or we could get the "default shipping address" and try that, and if that fails too,
+        // just iterate over the remaining ones, if any
+
+        // TODO: should we really use the default, like in this implementation? I wouldn't think so
         // Fetch the default shipping address
         $defaultShippingAddressId = $currentUser->getDefaultShipping();
         if ($defaultShippingAddressId) {
@@ -137,28 +163,51 @@ class KL_Klarna_Model_Klarnacheckout_BuildRecurringOrder
         return $klarnaData;
     }
 
+    /**
+     *
+     */
     private function getCountry()
     {
         // TODO:
     }
 
+    /**
+     *
+     */
     private function getCurrency()
     {
         // TODO:
     }
 
+    /**
+     *
+     */
     private function getLocale()
     {
         // TODO:
     }
 
+    /**
+     *
+     */
     private function getMerchantId()
     {
         // TODO:
     }
 
+    /**
+     *
+     */
     private function getQuote()
     {
         // TODO:
+    }
+
+    /**
+     * @param KL_Subscriber_Model_Quote $quote
+     */
+    private function setQuote(KL_Subscriber_Model_Quote $quote)
+    {
+        $this->quote = $quote;
     }
 } 
