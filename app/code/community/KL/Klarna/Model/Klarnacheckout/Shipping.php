7966e04d6f2e347075577b00724c4cb084b24b8d
David Wickström <davidwickstrom@gmail.com>
2015-05-27 14:13:47 +0200

    Shipping tax rate should be expressed in % times 100

--- app/code/community/KL/Klarna/Model/Klarnacheckout/Shipping.php
+++ app/code/community/KL/Klarna/Model/Klarnacheckout/Shipping.php
@@ -33,7 +33,7 @@ class KL_Klarna_Model_Klarnacheckout_Shipping extends KL_Klarna_Model_Klarnachec
             'quantity' => 1,
             'unit_price' => intval($shippingAddress->getShippingAmount() * 100),
             'discount_rate' => 0, // Not needed since Magento gives us the actual price
-            'tax_rate' => $this->calculateShippingTaxPercentage($quote),
+            'tax_rate' => $this->calculateShippingTaxPercentage($quote) * 100,
             'type' => 'shipping_fee'
         );
     }
