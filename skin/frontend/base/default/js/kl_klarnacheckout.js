8c7d973355f2fd44b469bbddabd100fcfc6e4744
Andreas Karlsson <andreas.karlsson@indiebytes.se>
2015-05-29 09:53:42 +0200

    Add javascript functionality from one of the projects using the module

--- skin/frontend/base/default/js/kl_klarnacheckout.js
+++ skin/frontend/base/default/js/kl_klarnacheckout.js
@@ -1,3 +1,38 @@
+// Abstract(s)
+function _klarnaCheckoutWrapper(callback) {
+    if (typeof _klarnaCheckout != 'undefined') {
+        _klarnaCheckout(function(api) {
+            api.suspend();
+            typeof callback === 'function' && callback(api);
+        });
+    }
+}
+
+// Helpers
+function klarnaCheckoutSuspend() {
+    _klarnaCheckoutWrapper();
+}
+
+function klarnaCheckoutResume() {
+    _klarnaCheckoutWrapper(function(api) {
+        api.resume();
+    });
+}
+
+function klarnaCheckoutSetShippingMethod(code) {
+    _klarnaCheckoutWrapper(function(api) {
+        new Ajax.Request('/klarna/shipping', {
+            method: 'POST',
+            parameters: {
+                shippingCode: code
+            },
+            onComplete: function(transport){
+                api.resume();
+            }
+        });
+    });
+}
+
 function klarnaCheckoutSwitchPayment(code)
 {
     window._klarnaCheckout(function (api) {
