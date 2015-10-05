251a505d5c9272c73514069f2a5c9bdab9521176
Andreas Karlsson <andreas.karlsson@indiebytes.se>
2015-10-05 08:16:41 +0200

    Add function to collect emails for abandoned carts Uses cron, poorly tested.

---
+++ app/code/community/KL/Klarna/Model/Cron/Quote.php
@@ -0,0 +1,40 @@
+<?php
+class KL_Klarna_Model_Cron_Quote
+{
+    protected function _getCollection()
+    {
+        $from = Mage::getModel('core/date')->gmtDate(null, date('Y-m-d H:i:s', strtotime('-4 hour')));
+        $to = Mage::getModel('core/date')->gmtDate(null, date('Y-m-d H:i:s', strtotime('-1 hour')));
+
+        $quotes = Mage::getModel('sales/quote')
+            ->getCollection()
+            ->addFieldToFilter('klarna_checkout', array('notnull' => true))
+            ->addFieldToFilter('customer_email', array('null' => true))
+            ->addFieldToFilter('updated_at', array('from' => $from))
+            ->addFieldToFilter('updated_at', array('to' => $to))
+            ->load();
+
+        return $quotes;
+    }
+
+    public function collectEmails()
+    {
+        
+        Mage::log('collectEmails', null, 'klarnaCollectEmails.log', true);
+
+        foreach ($this->_getCollection() as $quote)
+        {
+            $checkoutId = $quote->getKlarnaCheckout();
+
+            if ($checkoutId) {
+                $checkout = Mage::getModel('klarna/klarnacheckout')->getOrder($checkoutId);
+
+                if (isset($checkout['shipping_address']['email']) && $checkout['shipping_address']['email']) {
+                    Mage::log($quote->getId(), null, 'klarnaCollectEmails.log', true);
+                    $quote->setCustomerEmail($checkout['shipping_address']['email']);
+                    $quote->save();
+                }
+            }
+        }
+    }
+}
