431949d14607b4a9577abd84de575ececdda9da2
Andreas Karlsson <andreas.karlsson@indiebytes.se>
2016-01-07 16:08:33 +0100

    Add column to tables and method to create subscribtions

---
+++ app/code/community/KL/Klarna/sql/klklarna_setup/mysql4-upgrade-0.0.7-0.0.8.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @var Mage_Sales_Model_Resource_Setup $installer
+ */
+$installer = Mage::getResourceModel('sales/setup', 'default_setup');
+
+$installer->startSetup();
+
+$attributes = array(
+    'newsletter_subscription' => array(
+        'label' => 'Add for newsletter subscription',
+        'type' => 'boolean',
+    ),
+);
+
+$entities = [
+    'order',
+    'quote'
+];
+
+foreach ($entities as $entity) {
+    foreach ($attributes as $attributeCode => $data) {
+        $installer->addAttribute(
+            $entity,
+            $attributeCode,
+            $data
+        );
+    }
+}
+
+$installer->endSetup();
